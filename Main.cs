a998be8ede8e11de01759faea7722023e0f798ed
ECMiraldo <eduardomiraldo@gmail.com>
2020-12-05 16:19:34 +0000

    Adicionada uma possivel maneira de ler o ficheiro de texto

--- Main.cs
+++ Main.cs
@@ -4,5 +4,28 @@ public class Program
 {
 	public Program()
 	{
+		// Read each line of the file into a string array. Each element of the array is one line of the file.
+		//encontrado num documento da microsoft e feito o glorioso copy-paste
+		string[] lines = System.IO.File.ReadAllLines(@"C:\Users\Public\TestFolder\Ficheiro_que_o_prof_vai_mandar.txt");
+
+		/* 
+		exemplo de ficheiro de testo e consequentemente do array de strings - lines
+		2 E1 E2 21672
+		1 P E1 10501
+		1 E1 E2 37203
+		2 P E1 12383
+		1 E2 C 28465
+		2 E2 C 23567
+		*/
+
+		// ou seja, sera sempre 1 numero inteiro, seguido de um espaço, seguido de uma string(?) seguido de outro espaço, seguido por um tempo. Para cada etapa.
+
+
+		//sera que conseguimos transformar esses elementos da primeira string(que é uma linha toda) numa string normal(1 elemento = 1 caracter) assim?
+		string informacoes = (string)string[] lines;
+
+
+
+
 	}
 }
