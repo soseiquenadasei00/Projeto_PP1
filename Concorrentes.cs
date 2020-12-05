59d4a8e964ab6350b21aaecb780b91251c84aa1e
ECMiraldo <eduardomiraldo@gmail.com>
2020-12-05 15:33:47 +0000

    Inicei projeto: Declaração de variaveis, e construtor para classe concorrente

--- Concorrentes.cs
+++ Concorrentes.cs
@@ -1,8 +1,36 @@
 ﻿using System;
+using System.Collections;
 
-public class Concorrentes
+
+namespace trabalho_PP1
 {
-	public Concorrentes()
-	{
-	}
+
+    public class Concorrente
+    {
+        public int numero;
+        public int quantidade;
+        public string nome;
+        public string carro;
+        public int posicao;
+        public double tempoEtapa;
+        public double tempoTotal;
+
+
+        public Concorrente(int numero; string nome, string carro)
+            {
+                this.numero = numero;
+                this.nome = nome;
+                this.carro = carro;
+            }
+
+    public double CalcularTempo()
+    {
+        tempoTotal = tempoEtapa * numeroEtapas;
+    }
+
+    public bool ProvaValida(Concorrente c)
+    {
+
+    }
+}
 }
