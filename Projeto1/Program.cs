f48977284d0f60da2db0dce146ffc00f3f7d350e
Soseiquenadasei00 <pedrovsaldanha@gmail.com>
2021-01-09 13:03:40 +0000

    Menu (  fase de teste)

--- Projeto1/Program.cs
+++ Projeto1/Program.cs
@@ -7,16 +7,108 @@ namespace Projeto1
 	{
 		static void Main(string[] args)
 		{
-			Provas rally2 = new Provas();
-			Interface.LerConcorrentes(rally2, "concorrentes.txt");
-			try { Interface.LerEtapas(rally2, "etapas.txt"); 
-			
-			}
-			catch {
-				Console.WriteLine("Erro ao ler etapas, confira que nao escreveu lagartixa!");
+
+		    Provas rally2 = new Provas();
+
+			int opc = 1;
+
+			Console.WriteLine("Competição de carros do EDJD");
+			while (opc != 0)
+			{
+				Console.WriteLine("Por favor, introduza o nome do ficheiro com os concorrentes");
+				string ficheirosconcorrentes = Console.ReadLine();
+				try
+				{
+					Interface.LerConcorrentes(rally2, ficheirosconcorrentes);
+				}
+				catch
+				{
+					Console.WriteLine("POXA PROFESSOR, PRECISA COLOCAR O NOME CERTO.");
+				}
+
+				Console.WriteLine("Por favor, introduza o nome do ficheiro com as etapas");
+				string ficheirodasetapas = Console.ReadLine();
+				try
+				{
+					Interface.LerEtapas(rally2, ficheirodasetapas);
+				}
+				catch
+				{
+					Console.WriteLine("DE NOVO??? POXA PROFESSOR, PRECISA COLOCAR O NOME CERTO.");
+				}
+
+				Console.WriteLine("Por favor, introduza o nome do ficheiro com as distancias");
+				string ficheiroDasDistancias = Console.ReadLine();
+				try
+				{
+					Interface.LerDistancias(rally2, ficheiroDasDistancias);
+				}
+				catch
+				{
+					Console.WriteLine("BRINCADEIRA VIU!! ERROU DE NOVO??? COLOCA O NOME CERTO PELO AMOR");
+				}
+				Console.WriteLine("Verifique que não há neenhum erro, digite 0 para continuar");
+				opc = int.Parse(Console.ReadLine());
 			}
 
-			Interface.LerDistancias(rally2, "Distancias.txt");
+			Console.Clear();
+
+			Console.WriteLine();
+
+			Console.WriteLine("Competição de carros do EDJD");
+
+			Console.WriteLine("1-Para saber o número de concorrentes que estão em prova");
+			Console.WriteLine("2-Para saber o número de concorrentes que possuem as provas validas");
+			Console.WriteLine("3-Para saber o tempo de todos os concorrentes que efetuaram uma prova valida por ordem decrescente");
+			Console.WriteLine("4-Para saber as médias dos tempos por etapa");
+			Console.WriteLine("5-Pra saber quem foi o carro mais rápido e o mais lento na competição");
+			Console.WriteLine("6-Para saber qual foi a etapa mais lenta do melhor concorrente");
+			Console.WriteLine("7-Para saber qual é o menor tempo em que é possível efetuar a prova na totalidade independentemente de ser uma prova vailida ou não");
+			Console.WriteLine("8-Para saber quem foi o vencedor");
+			Console.WriteLine("9-Tabela classificativa da prova");
+			Console.WriteLine("0 - Sair");
+			int op;
+			op = int.Parse(Console.ReadLine());
+			do
+			{
+				switch (op)
+				{
+					case 1:
+						Console.WriteLine(rally2.NumeroConcorrentesEmProva());
+						break;
+
+					case 2:
+						Console.WriteLine(rally2.NumeroDeProvasValidas());
+						break;
+					case 3:
+						rally2.printTempoDecre();
+						break;
+					case 4:
+						Console.WriteLine(rally2.TempoDasEtapasParaProvasValidas());
+						break;
+					case 5:
+						Console.WriteLine("O carro mais rapido foi :" + rally2.CarroMaisRapido() + "O carro mais lento foi:" + rally2.CarroMaisLento());
+						break;
+					case 6:
+						Console.WriteLine("A etapa mais lenta do vencedor foi:" + rally2.PiorEtapaDoVencedor());
+						break;
+					case 7:
+						Console.WriteLine(rally2.MenorTempoPossivel());
+						break;
+					case 8:
+						Console.WriteLine(rally2.Vencedor());
+						break;
+					//case 9:Console.WriteLine();
+
+					default:
+						Console.WriteLine("Escreva o numero corretamente");
+						break;
+				}
+			} while (op != 0 && op < 10);
+			
+			return;
+
+
 
 			//Metodos da classe etapa
 
